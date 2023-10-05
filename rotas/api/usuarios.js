a0b589fbe9c4bea9a2090fb957ed77c3a3586c46
Adauto Donizete Junior - Business Intelligence Lev Negócios <analista.bi@levnegocios.com.br>
2023-10-05 17:14:45 -0300

    adicionando a rota para obter o usuario por id e adicao das rotas get by tipoUsuario e get by Id no swagger

--- rotas/api/usuarios.js
+++ rotas/api/usuarios.js
@@ -1,7 +1,8 @@
 // Importa as bibliotecas necessárias
 const express = require("express");
-const router = express.Router();
 const Usuario = require("../../modelos/Usuario");
+
+const router = express.Router();
 
 // Rota GET para obter todos os usuários
 router.get("/", async (req, res) => {
@@ -9,35 +10,45 @@ router.get("/", async (req, res) => {
     // Busca todos os usuários no banco de dados
     const usuarios = await Usuario.find();
     // Retorna os usuários encontrados
-    res.json(usuarios);
+    res.status(200).json(usuarios);
   } catch (err) {
     // Retorna uma resposta de erro caso algo dê errado
     console.error(err.message);
-    res.status(500).send("Erro no servidor");
+    res.status(500).json({"error": 'Erro no servidor'});
   }
 });
 
-// Rota GET para obter todos os usuários ou apenas um tipo específico
-router.get("/", async (req, res) => {
-  const { tipoUsuario } = req.query; // Pega o query param 'tipoUsuario'
+// Rota GET para obter usuários BY ID
+router.get("/id", async (req, res) => {
+  // Pega o query param 'id'
+  const { id } = req.query;
 
   try {
-    let usuarios;
-
-    if (tipoUsuario) {
-      // Busca todos os usuários de um tipo específico
-      usuarios = await Usuario.find({ tipoUsuario });
-    } else {
-      // Busca todos os usuários
-      usuarios = await Usuario.find();
-    }
-
+    // Busca o usuário correspondente ao ID
+    usuario = await Usuario.find({"_id": id})
     // Retorna os usuários encontrados
-    res.json(usuarios);
+    res.status(200).json(usuario)
   } catch (err) {
     // Retorna uma resposta de erro caso algo dê errado
     console.error(err.message);
-    res.status(500).send("Erro no servidor");
+    res.status(500).json({"error": 'Erro no servidor'});
+  }
+});
+
+// Rota GET para obter os usuários BY tipo de usuário
+router.get("/tipoUsuario", async (req, res) => {
+  // Pega o query param 'tipoUsuario'
+  const { tipoUsuario } = req.query;
+
+  try {
+    // Busca todos os usuários de um tipo específico
+    usuarios = await Usuario.find({ tipoUsuario });
+    // Retorna os usuários encontrados
+    res.status(200).json(usuarios);
+  } catch (err) {
+    // Retorna uma resposta de erro caso algo dê errado
+    console.error(err.message);
+    res.status(500).json({"error": 'Erro no servidor'});
   }
 });
 

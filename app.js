af4bcc9b9f7ae51ce4932c04755e86e3cd9a4c38
Adauto Donizete Junior - Business Intelligence Lev Negócios <analista.bi@levnegocios.com.br>
2023-10-05 16:07:56 -0300

    adicionando e configurando o swagger e documentando a rota get de usuarios

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // Importa as bibliotecas necessárias
+const cors = require("cors");
 const express = require("express");
 const conectarDB = require("./config/banco");
-const cors = require("cors");
+const swagger = require("swagger-ui-express");
+const swaggerDoc = require("./swagger.json")
 
 // Inicia o aplicativo Express
 const app = express();
@@ -14,6 +16,9 @@ app.use(cors());
 
 // Middleware para análise do corpo das requisições
 app.use(express.json({ extended: false }));
+
+// Configura a documentação da API Swagger para ser acessada em '/api/docs'.
+app.use('/api/docs', swagger.serve, swagger.setup(swaggerDoc));
 
 // Define as rotas da API
 app.use("/api/usuarios", require("./rotas/api/usuarios"));

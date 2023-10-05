0eb8dfe8f3b63d919f1fdd91eb4f96622eff4044
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2023-10-05 00:19:00 -0300

    adicionada rota de solicitacao por tipo de user

--- rotas/api/usuarios.js
+++ rotas/api/usuarios.js
@@ -1,10 +1,10 @@
 // Importa as bibliotecas necessárias
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Usuario = require('../../modelos/Usuario');
+const Usuario = require("../../modelos/Usuario");
 
 // Rota GET para obter todos os usuários
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // Busca todos os usuários no banco de dados
     const usuarios = await Usuario.find();
@@ -13,7 +13,31 @@ router.get('/', async (req, res) => {
   } catch (err) {
     // Retorna uma resposta de erro caso algo dê errado
     console.error(err.message);
-    res.status(500).send('Erro no servidor');
+    res.status(500).send("Erro no servidor");
+  }
+});
+
+// Rota GET para obter todos os usuários ou apenas um tipo específico
+router.get("/", async (req, res) => {
+  const { tipoUsuario } = req.query; // Pega o query param 'tipoUsuario'
+
+  try {
+    let usuarios;
+
+    if (tipoUsuario) {
+      // Busca todos os usuários de um tipo específico
+      usuarios = await Usuario.find({ tipoUsuario });
+    } else {
+      // Busca todos os usuários
+      usuarios = await Usuario.find();
+    }
+
+    // Retorna os usuários encontrados
+    res.json(usuarios);
+  } catch (err) {
+    // Retorna uma resposta de erro caso algo dê errado
+    console.error(err.message);
+    res.status(500).send("Erro no servidor");
   }
 });
 

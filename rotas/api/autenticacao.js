69567863b3d661e367e05b6ace3fca5908c58166
Adauto Donizete Junior - Business Intelligence Lev Negócios <analista.bi@levnegocios.com.br>
2023-10-05 19:54:17 -0300

    alterando o local da rota de inserção de usuário e adição dessa rota no swagger

--- rotas/api/autenticacao.js
+++ rotas/api/autenticacao.js
@@ -5,47 +5,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Usuario = require("../../modelos/Usuario");
 
-// Rota POST para registro de usuário
-router.post("/registro", async (req, res) => {
-  console.log("Rota /registro chamada");
-  console.log("Corpo da Requisição:", req.body);
-  // Desestruturação para obter os dados do corpo da requisição
-  const { nome, email, senha, endereco, tipoUsuario } = req.body;
-
-  try {
-    // Verifica se o e-mail já está em uso
-    let usuario = await Usuario.findOne({ email });
-    if (usuario) {
-      return res.status(400).json({ erros: [{ msg: "Usuário já existe" }] });
-    }
-
-    // Cria um novo objeto de usuário
-    usuario = new Usuario({
-      nome,
-      email,
-      senha,
-      endereco,
-      tipoUsuario,
-    });
-
-    // Criptografa a senha antes de salvar no banco de dados
-    const salt = await bcrypt.genSalt(10);
-    usuario.senha = await bcrypt.hash(senha, salt);
-
-    // Salva o usuário no banco de dados
-    await usuario.save();
-
-    // Retorna uma resposta de sucesso
-    res.status(201).json({ msg: "Usuário registrado com sucesso" });
-  } catch (err) {
-    // Retorna uma resposta de erro caso algo dê errado
-    console.error(err.message);
-    res.status(500).json({ msg: "Erro no servidor" });
-  }
-});
-
 // rotas/api/autenticacao.js
-
 router.post("/login", async (req, res) => {
   console.log("Rota /login chamada");
   const { email, senha } = req.body;
--- rotas/api/usuarios.js
+++ rotas/api/usuarios.js
@@ -1,5 +1,6 @@
 // Importa as bibliotecas necessárias
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const Usuario = require("../../modelos/Usuario");
 
 const router = express.Router();
@@ -45,6 +46,44 @@ router.get("/tipoUsuario", async (req, res) => {
     usuarios = await Usuario.find({ tipoUsuario });
     // Retorna os usuários encontrados
     res.status(200).json(usuarios);
+  } catch (err) {
+    // Retorna uma resposta de erro caso algo dê errado
+    console.error(err.message);
+    res.status(500).json({"error": 'Erro no servidor'});
+  }
+});
+
+// Rota POST para registro de usuário
+router.post("/", async (req, res) => {
+  // Desestruturação para obter os dados do corpo da requisição
+  const { nome, email, senha, endereco, tipoUsuario } = req.body;
+
+  try {
+    // Verifica se o e-mail já está em uso
+    let usuario = await Usuario.findOne({ email });
+    // Retorna um erro se o usuário já existir
+    if (usuario) {
+      return res.status(400).json({ erros: [{ msg: "Usuário já existe" }] });
+    }
+
+    // Cria um novo objeto de usuário
+    usuario = new Usuario({
+      nome,
+      email,
+      senha,
+      endereco,
+      tipoUsuario,
+    });
+
+    // Criptografa a senha antes de salvar no banco de dados
+    const salt = await bcrypt.genSalt(10);
+    usuario.senha = await bcrypt.hash(senha, salt);
+
+    // Salva o usuário no banco de dados
+    await usuario.save();
+
+    // Retorna uma resposta de sucesso
+    res.status(201).json();
   } catch (err) {
     // Retorna uma resposta de erro caso algo dê errado
     console.error(err.message);
